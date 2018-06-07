12a657ed9a554d7ce377c033f6e2f66a6368885e
zhoukun.com <915684080@qq.com>
2018-06-07 20:34:45 +0800

    fuck

---
+++ test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+#coding:utf-8
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run()
